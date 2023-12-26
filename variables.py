419fcc55a8fb1c6ffbfbe3c4ecc2c84efd303657
Roseizar <112078974+Roseizar@users.noreply.github.com>
2023-12-26 10:31:15 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,24 @@
+maasAli = 5000
+maasAhmet = 4000
+vergi =0.27
+print(maasAli- (maasAli*vergi))
+print(maasAhmet- (maasAhmet*vergi))
+ # Değişken TANIMLAMA KURALLARI
+number1 =10 
+print(number1)
+number2=10
+# büyük kücük harf duyarlılığı
+age=20
+AGE= 30
+print(age)
+x = 1 #int
+y = 2.3 #float
+name = "Çınar" #string
+isStudent = True #bool
+a = "10"
+b ="20"
+print(a+b)
+
+
+
+
